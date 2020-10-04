5e50c2f1c12fb49f1d07f5a363eafd7101ea446b
ian <iangabaratti@icloud.com>
2020-10-04 23:43:28 +0300

    license

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="psstore4-ru",
+    version="0.0.1",
+    author="Ian Gabaraev",
+    author_email="hrattisianees@gmail.com",
+    description="Play Station 4 Store Russian Python Interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ian-Gabaraev/psstorereader",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
