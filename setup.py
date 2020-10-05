350a957048504acd256f6ae0bd85415c686d656c
ian <iangabaratti@icloud.com>
2020-10-05 18:44:36 +0300

    upd setup

--- setup.py
+++ setup.py
@@ -18,5 +18,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+       'requests',
+       'bs4',
+    ],
     python_requires='>=3.6',
 )
